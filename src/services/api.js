0fc4fabbf3490b4a2b00231987b7a79eb077dbcf
Patrick <patrick.crf22@gmail.com>
2021-07-28 18:34:49 -0300

    correction cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,11 @@
-const BASE_URL = "https://www.receitaws.com.br/v1/cnpj/";
-//
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/https://www.receitaws.com.br/v1/cnpj/";
 
 export const api = {
   async get(endpoint) {
-    const response = await fetch(
-      `https://cors-anywhere.herokuapp.com/${BASE_URL}${endpoint}`,
-      {
-        method: "get",
-        headers: {
-          Authorization: "Bearer" + process.env.VUE_APP_API_KEY,
-          Origin: "https://www.receitaws.com.br",
-        },
-      }
-    );
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+      headers: { Origin: "www.receitaws.com.br/" },
+    });
     const json = await response.json();
     return json;
   },
